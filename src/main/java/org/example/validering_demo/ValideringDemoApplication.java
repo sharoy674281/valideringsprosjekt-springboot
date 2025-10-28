b8443735b6540aae667048c606f7ee873a648e9f
Rakhman Ismailov <rakhmanirs@yahoo.no>
2025-10-28 13:05:07 +0100

    first commit

---
+++ src/main/java/org/example/validering_demo/ValideringDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.validering_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValideringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValideringDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/validering_demo/ValideringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.validering_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValideringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
